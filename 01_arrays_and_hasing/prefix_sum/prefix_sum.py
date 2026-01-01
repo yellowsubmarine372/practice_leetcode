2efae532d5d14d11a565fe60c76d35b9f0640df3
yellowsubmarine372 <95083164+yellowsubmarine372@users.noreply.github.com>
2026-01-01 19:29:07 +0900

    fixed wrong calculations

--- 01_arrays_and_hasing/prefix_sum/prefix_sum.py
+++ 01_arrays_and_hasing/prefix_sum/prefix_sum.py
@@ -4,7 +4,5 @@ class PrefixSum:
     self.prefix_sum = [0] * (self.length + 1) # first is a dummy node
 
   def calculate(self, arr: list) -> None:
-    self.prefix_sum[0] = arr[0]
-
     for i in range(self.length):
       self.prefix_sum[i+1] = self.prefix_sum[i] + arr[i]
