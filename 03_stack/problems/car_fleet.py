92726d912e118898b59dd2e424cf54a9df32d9cb
yellowsubmarine372 <95083164+yellowsubmarine372@users.noreply.github.com>
2026-02-05 20:33:55 +0900

    modified wrong logic

--- 03_stack/problems/car_fleet.py
+++ 03_stack/problems/car_fleet.py
@@ -7,7 +7,7 @@ class Solution:
     stack = []
 
     for p, s in sorted(pair)[::-1]:
-      a = (target - p)/s
-      if stack and a <= stack[-1]:
+      stack.append((target - p)/s)
+      if len(stack) >= 2 and stack[-1] <= stack[-2]:
         stack.pop()
     return len(stack)
