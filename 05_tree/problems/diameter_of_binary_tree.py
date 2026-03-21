778ec9c1297d0a35d8155d4c1ebb3f3f178413f3
yellowsubmarine372 <95083164+yellowsubmarine372@users.noreply.github.com>
2026-03-21 21:26:43 +0900

    day 57 of leetcode2

---
+++ 05_tree/problems/diameter_of_binary_tree.py
@@ -0,0 +1,27 @@
+from typing import Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        self.res = 0 # so it is accessible on the dfs()
+
+        # return height
+        def dfs(curr):
+            if not curr:
+                return 0
+            left = dfs(curr.left)
+            right = dfs(curr.right)
+
+            # 1) and 2) are different processes
+            self.res = max(self.res, left + right) # 1) this line is for calculating maxDiameter
+            return 1 + max(left, right) # 2) this line is for calculating left/right subtree height - so it only chooses one
+            # 1 means the edge (to left OR right - not simultaneously)
+        
+        dfs(root)
+        return self.res
