3cc935540fd10f362bbd7ba70b93f7b3323ec14b
yellowsubmarine372 <95083164+yellowsubmarine372@users.noreply.github.com>
2026-03-23 23:36:39 +0900

    day 59 of leetcode

---
+++ 05_tree/problems/same_tree.py
@@ -0,0 +1,22 @@
+from typing import Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        def dfs(p, q):
+            if p is None and q is None:
+                return True
+            if p is None or q is None:
+                return False
+            if p.val != q.val:
+                return False
+
+            return dfs(p.left, q.left) and dfs(p.right, q.right)
+
+        return dfs(p, q)
